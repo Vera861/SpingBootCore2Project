ea0bb5161cb095c1c911f21c83eeedd1f6f931a3
Vera Meshkova <mverynchik@gmail.com>
2021-10-12 23:50:46 +0300

    fiks

--- src/main/java/com/geekbrains/project/frontend/MainView.java
+++ src/main/java/com/geekbrains/project/frontend/MainView.java
@@ -1,26 +1,76 @@
 package com.geekbrains.project.frontend;
 
+import com.geekbrains.project.entity.Product;
+import com.geekbrains.project.service.ProductService;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 @Route("main")
 public class MainView extends VerticalLayout {
-    public MainView(){
+    private final Grid<Product> productGrid = new Grid<>(Product.class);
 
-        HorizontalLayout titleLayout = new HorizontalLayout();
-        HorizontalLayout buttonTextLayout = new HorizontalLayout();
-        Text titleText = new Text("Интернет-магазин");
-        Text buttonText = new Text("Здесь есть кнопка");
+    private final ProductService productService;
 
-        Button button = new Button("Нажми меня",event ->  {
-            Notification.show("Мы нажали кнопку");
+    public MainView(ProductService productService) {
+        this.productService = productService;
+
+        initPage();
+    }
+
+    private void initPage() {
+        var cartLayout = initCartButton();
+        initProductGrid();
+
+        add(productGrid, cartLayout);
+    }
+
+    private HorizontalLayout initCartButton() {
+        var addToCardButton = new Button("Добавить в корзину", event -> {
+            //TODO: Сохранение в бд какому-либо пользователю
+            Notification.show("Товар успешно добавлен в корзину");
         });
-        titleLayout.add(titleText);
-        buttonTextLayout.add(buttonText,button);
-        add(titleLayout, buttonTextLayout);
+
+        var toCartButton = new Button("Корзина", event -> {
+            UI.getCurrent().navigate("cart");
+        });
+
+        return new HorizontalLayout(addToCardButton, toCartButton);
+    }
+
+    private void initProductGrid() {
+        var products = productService.getAll();
+
+        productGrid.setItems(products);
+        productGrid.setColumns("name", "price", "count", "vendorCode");
+        productGrid.setSizeUndefined();
+        productGrid.setSelectionMode(Grid.SelectionMode.MULTI);
+
+        ListDataProvider<Product> dataProvider = DataProvider.ofCollection(products);
+        productGrid.setDataProvider(dataProvider);
+
+        productGrid.addColumn(new ComponentRenderer<>(item -> {
+            var plusButton = new Button("+", i -> {
+                item.incrementCount();
+                productService.save(item);
+                productGrid.getDataProvider().refreshItem(item);
+            });
+
+            var minusButton = new Button("-", i -> {
+                item.decreaseCount();
+                productService.save(item);
+                productGrid.getDataProvider().refreshItem(item);
+            });
+
+            return new HorizontalLayout(plusButton, minusButton);
+        }));
     }
 }
--- src/main/java/com/geekbrains/project/model/Product.java
+++
@@ -1,4 +0,0 @@
-package com.geekbrains.project.model;
-
-public class Product {
-}
